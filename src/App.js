04e5149a063c1c4c500a45d86df100ec6f76b048
roma <roma.saipov@bk.ru>
2023-04-25 17:41:26 +0300

    devoloper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Footer from './components/footer/Footer';
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
           <Route path='/'  element={<Home />} />
 
         </Routes>  
+
+         <Footer />
 
     </div>
     
